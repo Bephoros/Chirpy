0eb920a2ad46074e5fa0e27d162589ab67bc9e2f
Bephoros <bephorosmain@gmail.com>
2025-01-18 16:25:56 -0600

    Started simple server

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	//"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	s.ListenAndServe()
+}
